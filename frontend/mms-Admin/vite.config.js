61738149ad91ebf02cfee66dd19c1a3463374b8f
Calistus Igwilo <ebunilo@yahoo.com>
2023-05-01 18:29:06 +0100

    vite server port

--- frontend/mms-Admin/vite.config.js
+++ frontend/mms-Admin/vite.config.js
@@ -1,22 +1,23 @@
-import { defineConfig, loadEnv  } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, process.cwd());
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
 
-  return ({
-    plugins: [react(), svgr()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src")
-      }
-    },
-    server: {
-      host: env.VITE_HOST,
-      port: env.VITE_PORT // The host port the app will be accessed from
-    }
-  });
+    return ({
+        plugins: [react(), svgr()],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src")
+            }
+        },
+        server: {
+            host: true,
+            strictPort: true,
+            port: 3001 // The host port the app will be accessed from
+        }
+    });
 });
